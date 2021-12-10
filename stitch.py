e11c29fba20785232fbd35bec2c859f9f56403b0
manu@epinardscaramel.eu <manu@epinardscaramel.eu>
2021-12-10 22:02:11 +0100

    - appuyer sur Entrée pour passer du mode « ligne » au mode « colonne »

--- stitch.py
+++ stitch.py
@@ -22,7 +22,7 @@ COULEUR_POINTS_TRAITS = 'black'
 TAILLE_TRAITS = 5
 INTERVALLE = TAILLE_ECRAN//(TAILLE_MOTS+2)
 
-def draw(mot_1, mot_2):
+def draw(mot_1: str, mot_2: str, x_actif: bool):
 	screen.fill('white')
 
 	# grille
@@ -33,16 +33,23 @@ def draw(mot_1, mot_2):
 	mot_1 = f'{mot_1:<{TAILLE_MOTS}}'
 	mot_2 = f'{mot_2:<{TAILLE_MOTS}}'
 
+	if x_actif:
+		couleur_x = 'blue'
+		couleur_y = 'black'
+	else:
+		couleur_x = 'black'
+		couleur_y = 'blue'
+
 	# lignes
 	for n,lettre in enumerate(mot_2):
-		screen.blit(POLICE_MOTS.render(lettre, 1, 'black'), (INTERVALLE//3, INTERVALLE-15+n*INTERVALLE))
+		screen.blit(POLICE_MOTS.render(lettre, 1, couleur_x), (INTERVALLE//3, INTERVALLE+n*INTERVALLE))
 		if not lettre == ' ':
 			start_on = unicodedata.normalize('NFKD', lettre)[0] in ('a')
 			draw_ligne(n+1,start_on)
 
 	# colonnes
 	for n,lettre in enumerate(mot_1):
-		screen.blit(POLICE_MOTS.render(lettre, 1, 'blue'), (INTERVALLE-8+n*INTERVALLE, 0))
+		screen.blit(POLICE_MOTS.render(lettre, 1, couleur_y), (INTERVALLE+15+n*INTERVALLE, 0))
 		if not lettre == ' ':
 			start_on = unicodedata.normalize('NFKD', lettre)[0] in ('aeiouy')
 			draw_colonne(n+1,start_on)
@@ -52,19 +59,25 @@ def draw(mot_1, mot_2):
 	pygame.display.update()
 
 
-def draw_ligne(ligne, start_on):
+def draw_ligne(ligne: int, start_on: bool):
 	for colonne in range(0,TAILLE_MOTS):
 		if start_on:
 			pygame.draw.rect(screen, COULEUR_POINTS_TRAITS,
 				pygame.Rect(INTERVALLE+colonne*INTERVALLE, ligne*INTERVALLE-(TAILLE_TRAITS/2), INTERVALLE+TAILLE_TRAITS,10))
 		start_on = not start_on
 
-def draw_colonne(colonne, start_on):
+def draw_colonne(colonne: int, start_on: bool):
 	for ligne in range(0,TAILLE_MOTS):
 		if start_on:
 			pygame.draw.rect(screen, COULEUR_POINTS_TRAITS,
 				pygame.Rect(colonne*INTERVALLE-(TAILLE_TRAITS/2), INTERVALLE+ligne*INTERVALLE-(TAILLE_TRAITS/2), 10,INTERVALLE+2*TAILLE_TRAITS))
 		start_on = not start_on
+
+def completer_mot(mot: str, caractere: str) -> str:
+	mot = mot + caractere
+	if len(mot)>TAILLE_MOTS-1:
+		mot = mot[1:]
+	return mot
 
 def main():
 	clock = pygame.time.Clock()
@@ -73,8 +86,9 @@ def main():
 	if DEBUG:
 		print('intervale = {:d}'.format(INTERVALLE))
 
-	mot_x = mot_y = ''#' ' * TAILLE_MOTS
-	draw(mot_x, mot_y)
+	mot_x = mot_y = ''
+	mode_colonne = False
+	draw(mot_x, mot_y, mode_colonne)
 
 	while jouer:
 		clock.tick(FPS)
@@ -89,16 +103,23 @@ def main():
 					pygame.quit()
 					sys.exit()
 
-				mot_x = mot_x + event.unicode
-				if len(mot_x)>TAILLE_MOTS:
-					mot_x = mot_x[1:]
+				elif (event.key == pygame.K_KP_ENTER) or (event.key == pygame.K_RETURN):
+					mode_colonne = not mode_colonne
 
-				mot_y = mot_y + event.unicode
-				if len(mot_y)>TAILLE_MOTS-1:
-					mot_y = mot_y[1:]
+				elif event.key == pygame.K_BACKSPACE:
+					if mode_colonne:
+						mot_y = mot_y[:-1]
+					else:
+						mot_x = mot_x[:-1]
+
+				else:
+					if mode_colonne:
+						mot_y = completer_mot(mot_y, event.unicode)
+					else:
+						mot_x = completer_mot(mot_x, event.unicode)
 
 				# Visuals : draw()
-				draw(mot_x, mot_y)
+				draw(mot_x, mot_y, mode_colonne)
 
 if __name__ == "__main__":
 	main()
