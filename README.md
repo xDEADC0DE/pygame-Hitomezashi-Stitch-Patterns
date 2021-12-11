05d2a15a1c98ca7c8c2be20f03adf48597965f6c
manu@epinardscaramel.eu <manu@epinardscaramel.eu>
2021-12-11 14:16:50 +0100

    * FIXES #4 pouvoir changer TAILLE_MOTS depuis l’application * FIXES #2 revoir le placement des lettres pour différentes valeurs de TAILLE_MOTS * revu l’affichage de la grille, sans ajouter de colonnes ou lignes superflues

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Hitomezashi Stitch Patterns
 2<sup>e</sup> projet avec Pygame
 
-Génère des motifs intéressants en fonction des lettres tapées. Appuyez sur `Entrée` pour passer du mode «&nbsp;ligne&nbsp;» au mode «&nbsp;colonne&nbsp;»
+Génère des motifs intéressants en fonction des lettres tapées.
 
-![capture d’écran](https://github.com/xDEADC0DE/pygame-Hitomezashi-Stitch-Patterns/blob/master/screenshots/2021-12-10%2022h20.png?raw=true)
+![capture d’écran](https://github.com/xDEADC0DE/pygame-Hitomezashi-Stitch-Patterns/blob/master/screenshots/2021-12-11%2014h00.png?raw=true)
 
+# contrôles
+* Appuyez sur `Entrée` pour passer du mode «&nbsp;ligne&nbsp;» au mode «&nbsp;colonne&nbsp;»
+* `+` et `-` permettent de changer la taille de la grille
+
+# notes
 Inspiré de la vidéo éponyme de Numberphile : https://www.youtube.com/watch?v=JbfhzlMk2eY
--- stitch.py
+++ stitch.py
@@ -3,7 +3,7 @@ import sys
 import unicodedata
 
 # configuration Pygame
-DEBUG = True
+DEBUG = False
 TAILLE_ECRAN = 900
 TITRE = 'Hitomezashi Stitch Patterns'
 FPS = 60
@@ -16,22 +16,28 @@ pygame.font.init()
 POLICE_MOTS = pygame.font.SysFont('arial', 30)
 
 # options du jeu
-TAILLE_MOTS = 16
+TAILLE_MOT_DEPART = 25
+TAILLE_MOT_MINI = 3
+TAILLE_MOT_MAXI = 50
+MODE_COLONNE_DEPART = False
 COULEUR_POINTS_GRILLE = 'gray'
 COULEUR_POINTS_TRAITS = 'black'
 TAILLE_TRAITS = 5
-INTERVALLE = TAILLE_ECRAN//(TAILLE_MOTS+2)
+
+taille_mot = TAILLE_MOT_DEPART
 
 def draw(mot_1: str, mot_2: str, x_actif: bool):
+	intervalle = TAILLE_ECRAN//(taille_mot+1)
+
 	screen.fill('white')
 
 	# grille
-	for colonne in range(INTERVALLE, TAILLE_ECRAN, INTERVALLE):
-		for ligne in range(INTERVALLE, TAILLE_ECRAN, INTERVALLE):
-			pygame.draw.rect(screen, COULEUR_POINTS_GRILLE, pygame.Rect(colonne,ligne, TAILLE_TRAITS,TAILLE_TRAITS))
+	for colonne in range(0, taille_mot):
+		for ligne in range(0, taille_mot):
+			pygame.draw.rect(screen, COULEUR_POINTS_GRILLE, pygame.Rect(intervalle+colonne*intervalle,intervalle+ligne*intervalle, TAILLE_TRAITS,TAILLE_TRAITS))
 
-	mot_1 = f'{mot_1:<{TAILLE_MOTS}}'
-	mot_2 = f'{mot_2:<{TAILLE_MOTS}}'
+	mot_1 = f'{mot_1:<{taille_mot}}'
+	mot_2 = f'{mot_2:<{taille_mot}}'
 
 	if x_actif:
 		couleur_x = 'blue'
@@ -43,36 +49,39 @@ def draw(mot_1: str, mot_2: str, x_actif: bool):
 	# lignes
 	for n,lettre in enumerate(mot_2):
 		texte = POLICE_MOTS.render(lettre, 1, couleur_x)
-		screen.blit(texte, (INTERVALLE//3, texte.get_height()+ n*INTERVALLE))
+		screen.blit(texte, (intervalle//3, (intervalle-texte.get_height()//2) + (n*intervalle)))
 		if not lettre == ' ':
-			start_on = unicodedata.normalize('NFKD', lettre)[0] in ('aeiouy')
-			draw_ligne(n+1,start_on)
+			start_on = unicodedata.normalize('NFKD', lettre)[0] in ('aeiouy02468')
+			draw_ligne(intervalle, n+1,start_on)
 
 	# colonnes
 	for n,lettre in enumerate(mot_1):
 		texte = POLICE_MOTS.render(lettre, 1, couleur_y)
-		screen.blit(texte, (INTERVALLE-texte.get_width()//2+n*INTERVALLE, 0))
+		screen.blit(texte, ((intervalle-texte.get_width()//2) + (n*intervalle), 0))
 		if not lettre == ' ':
-			start_on = unicodedata.normalize('NFKD', lettre)[0] in ('aeiouy')
-			draw_colonne(n+1,start_on)
+			start_on = unicodedata.normalize('NFKD', lettre)[0] in ('aeiouy02468')
+			draw_colonne(intervalle, n+1,start_on)
 
-	# unicode normalize
-	#u"".join([c for c in unicodedata.normalize('NFKD', event.unicode) if not unicodedata.combining(c)])
+	if DEBUG:
+		print()
+		print(f'taille_mot : {taille_mot}')
+		print(f'intervale : {intervalle}')
+
 	pygame.display.update()
 
 
-def draw_ligne(ligne: int, start_on: bool):
-	for colonne in range(0,TAILLE_MOTS):
+def draw_ligne(intervalle: int, ligne: int, start_on: bool):
+	for colonne in range(0,taille_mot-1):
 		if start_on:
 			pygame.draw.rect(screen, COULEUR_POINTS_TRAITS,
-				pygame.Rect(INTERVALLE+colonne*INTERVALLE, ligne*INTERVALLE-(TAILLE_TRAITS/2), INTERVALLE+TAILLE_TRAITS,10))
+				pygame.Rect(intervalle+colonne*intervalle, ligne*intervalle-(TAILLE_TRAITS/2), intervalle+TAILLE_TRAITS,10))
 		start_on = not start_on
 
-def draw_colonne(colonne: int, start_on: bool):
-	for ligne in range(0,TAILLE_MOTS):
+def draw_colonne(intervalle: int, colonne: int, start_on: bool):
+	for ligne in range(0,taille_mot-1):
 		if start_on:
 			pygame.draw.rect(screen, COULEUR_POINTS_TRAITS,
-				pygame.Rect(colonne*INTERVALLE-(TAILLE_TRAITS/2), INTERVALLE+ligne*INTERVALLE-(TAILLE_TRAITS/2), 10,INTERVALLE+2*TAILLE_TRAITS))
+				pygame.Rect(colonne*intervalle-(TAILLE_TRAITS/2), intervalle+ligne*intervalle-(TAILLE_TRAITS/2), 10,intervalle+2*TAILLE_TRAITS))
 		start_on = not start_on
 
 def completer_mot(mot: str, caractere: str) -> str:
@@ -81,19 +90,19 @@ def completer_mot(mot: str, caractere: str) -> str:
 		return mot
 		
 	mot = mot + caractere
-	if len(mot)>TAILLE_MOTS:
+	if len(mot)>taille_mot:
 		mot = mot[1:]
 	return mot
 
 def main():
+	global taille_mot
+
 	clock = pygame.time.Clock()
 	jouer = True
 
-	if DEBUG:
-		print('intervale = {:d}'.format(INTERVALLE))
-
 	mot_x = mot_y = ''
-	mode_colonne = False
+
+	mode_colonne = MODE_COLONNE_DEPART
 	draw(mot_x, mot_y, mode_colonne)
 
 	while jouer:
@@ -108,6 +117,14 @@ def main():
 				if event.key == pygame.K_ESCAPE:
 					pygame.quit()
 					sys.exit()
+
+				elif (event.key == pygame.K_KP_PLUS) or (event.key == pygame.K_PLUS):
+					if taille_mot < TAILLE_MOT_MAXI:
+						taille_mot +=1
+
+				elif (event.key == pygame.K_KP_MINUS) or (event.key == pygame.K_MINUS):
+					if taille_mot > TAILLE_MOT_MINI:
+						taille_mot -=1
 
 				elif (event.key == pygame.K_KP_ENTER) or (event.key == pygame.K_RETURN):
 					mode_colonne = not mode_colonne
